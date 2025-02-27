07f2258f3cfe19b09fee6f67d5da6f3a038c09d3
Єгор <yegork23@gmail.com>
2025-02-27 09:47:19 +0200

    Площа прямокутника

---
+++ src/main/java/test/RectangleArea.java
@@ -0,0 +1,14 @@
+package test;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        double width = 50.5;    // Ширина
+        double length = 25.5;    // Довжина
+        // Обчислення площі за формулою: Площа = Ширина * Довжина
+        double area = width * length;
+        // Виведення результату на екран
+        System.out.println("Ширина: " + width);
+        System.out.println("Довжина: " + length);
+        System.out.println("Площа прямокутника: " + area);
+    }
+}
