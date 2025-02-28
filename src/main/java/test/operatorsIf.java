c73ea847525e9dff1072cbeb8b1c5bd7ea5b28d3
Єгор <yegork23@gmail.com>
2025-02-28 22:45:13 +0200

    Пройшов цикли if else else if while do while and for та виправив помилки

--- src/main/java/test/operatorsIf.java
+++ src/main/java/test/operatorsIf.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class operatorsIf {
     public static void main (String[] args) {
-        Scanner skan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
        // int num = 1998;
         // if (num >= 1999)
@@ -20,16 +20,16 @@ public class operatorsIf {
         int price_car_citroen = 5000;
         int price_car_bmw = 23000;
         int price_car_ferrari = 90000;
-        int salery = 90000;
-        if (salery == price_car_citroen)
+        int salary = 90000;
+        if (salary == price_car_citroen)
         {
         System.out.println("Тоді я можу купити авто Сітроєн");
         }
-        else if (salery == price_car_bmw)
+        else if (salary == price_car_bmw)
         {
             System.out.println("Тоді я можу купити авто бмв");
         }
-        else if (salery == price_car_ferrari)
+        else if (salary == price_car_ferrari)
         {
             System.out.println("Тоді я можу купити авто Ферарі");
         }
@@ -42,7 +42,7 @@ public class operatorsIf {
         while (word.equals("Кола") != true)
         {
             System.out.println("Хочу лимонад але не знаю який");
-            word = skan.nextLine();
+            word = scanner.nextLine();
         }
         System.out.println("Ти вгадав");
 
