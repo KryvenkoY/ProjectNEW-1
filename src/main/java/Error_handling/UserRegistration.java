54ead2cb89fd51e81cd6840b0a4c20ab1a5a23bd
Єгор <yegork23@gmail.com>
2025-03-09 14:49:31 +0200

    Practical task InvalidAgeException

--- src/main/java/Error_handling/UserRegistration.java
+++ src/main/java/Error_handling/UserRegistration.java
@@ -1,4 +1,22 @@
 package Error_handling;
-
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
 public class UserRegistration {
+    public static void registerUser(String name, int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("(❌ Error: User " + name + " too young! Age must be 18+.");
+        }
+        System.out.println("✅ Registration successful pornHub.com ! Welcome, " + name + "!");
+    }
+    public static void main(String[] args) {
+        try {
+            registerUser("Yegor", 31);
+            registerUser("Mark", 17);
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
