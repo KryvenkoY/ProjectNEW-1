e1af48f38a33c6ae16c9dc34f4c35a76995e0c98
Єгор <yegork23@gmail.com>
2025-03-09 14:47:47 +0200

    Topic Unchecked exceptions

--- src/main/java/Error_handling/Unchecked_Exceptions.java
+++ src/main/java/Error_handling/Unchecked_Exceptions.java
@@ -1,4 +1,23 @@
 package Error_handling;
-
+// 1️⃣ Створюємо власний виняток (наприклад, некоректний номер картки)
+class InvalidCardException extends RuntimeException {
+    public InvalidCardException(String message) {
+        super(message);
+    }
+}
+// 2️⃣ Використовуємо цей виняток у методі
 public class Unchecked_Exceptions {
+    public static void validateCard(String cardNumber) {
+        if (cardNumber == null || cardNumber.length() !=16) {
+            throw new InvalidCardException("❌ Невірний номер картки! Введіть 16 цифр.");
+        }
+        System.out.println("✅ Картка " + cardNumber + " валідна.");
+    }
+    public static void main(String[] args) {
+        try {
+            validateCard("123");
+        } catch (InvalidCardException e) {
+            System.out.println("Помилка: " + e.getMessage());
+        }
+    }
 }
